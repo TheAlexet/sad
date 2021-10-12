ddecef1968c5ec88583035942f5a115405a8511b
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-10-12 19:26:09 +0200

    Comments added

--- my_modules/shoppingTrolley/mongoClient.js
+++ my_modules/shoppingTrolley/mongoClient.js
@@ -7,16 +7,23 @@ var url='mongodb+srv://dbUser:dbUserPassword@cluster0.oiahf.mongodb.net/myFirstD
 
 var collection;
 
+
+//Function used to check if there is enough stock of a product to add it to the trolley
+//Function made using promises
 exports.checkStock = function(productId, number){
   return new Promise((resolve,reject) => {
+    //We connect to the database
     mongoclient.connect(url,function (err,db) {
       assert.equal(err,null);
-      var dbo = db.db('shopping');
+      //We create a database object to be able to use it
+      var dbo = db.db('shopping'); 
       var collection = dbo.collection('products');
-      collection.find({id: productId}).toArray(function(err, productsList){
+      //We search the required product in the database
+      collection.find({id: productId}).toArray(function(err, productsList){ 
         var result = productsList[0];
         var amount = result.amount;
-        resolve(number < amount);
+        //The result of the promise is a boolean telling if the stock in the database is bigger than the amount asked
+        resolve(number < amount); 
         db.close();
       });
       
@@ -24,16 +31,20 @@ exports.checkStock = function(productId, number){
   });
 }
 
+//Function used to add products to the database
+//Function made using promises
 exports.insertProducts = function() {
   return new Promise((resolve, reject) => {
+    //We connect to the database
     mongoclient.connect(url,function (err,db) {
       assert.equal(err,null);
+      //We create a database object to be able to use it
       var dbo = db.db('shopping');
-      // Get the documents collection
+      // Get the products collection
       collection = dbo.collection('products');
       //Clean the database
       collection.deleteMany({});
-      // Insert some documents
+      // Insert some products
       collection.insertMany([
         {id: 1, pName: "Agua", description: "Bronchales 12L", price: 2.35, amount: 5}, 
         {id: 2, pName: "Pizza barbacoa", description: "Pizza barbacoa Cassa Tarradellas", price: 2.93, amount: 5}, 
@@ -42,6 +53,7 @@ exports.insertProducts = function() {
         {id: 5, pName: "Mascarilla", description: "#Stay home", price: 0.5, amount: 5}
       ], function(err, result) {
         assert.equal(err, null);
+        //The promise returns a message telling that the addition of the products was succesful
         resolve("Inserted 5 products into the collection");
         db.close();
       });
